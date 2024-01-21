3665ba25ed89cd3a948fa72fc76d1df5ce298b9e
patty lee <seunghak2003@gmail.com>
2024-01-21 15:00:02 -0500

    Reworked/Reorganized most of the logic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let playerScore = 0;
 let computerScore = 0;
 let winner = '';
@@ -74,7 +75,7 @@ function playRound(playerChoice, computerChoice) {
 }
 
 function game() {
-    playRound(getBtnPlayerChoice(), getComputerChoice())
+    playRound(getPlayerChoice(), getComputerChoice())
     if (endCheck(playerScore, computerScore) == 'w') {
       alert('You won!');
     } else if (endCheck(playerScore, computerScore) == 'l') {
@@ -96,4 +97,80 @@ function endCheck(playerScore, computerScore) {
 
 
 game()
+getBtnPlayerChoice();
+*/
+const pSCore = document.querySelector('.pSCore');
+const cSCore = document.querySelector('.cSCore');
+playerScore = pSCore.value;
+computerScore = cSCore.value;
+
+function getBtnPlayerChoice() {
+  const buttons = document.querySelectorAll('.rpc');
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      game(button.id);
+    });
+  });
+}
+
+function getComputerChoice() {
+  // goal is to create function that will choose between options
+  // generate a random pick, out of three
+  let randNumb = Math.floor(Math.random() * 3) + 1;
+  return randNumb == 1 ? 'Rock'
+    : randNumb == 2 ? 'Paper'
+    : 'Scissors';
+}
+
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    console.log(playerChoice);
+    console.log(computerChoice);
+    console.log('Tied!');
+    return 't'
+
+  } else if (playerChoice === 'Rock' && computerChoice === 'Scissors'){
+    console.log(playerChoice);
+    console.log(computerChoice);
+    console.log('You won!');
+    playerScore++;
+    return 'w'
+
+  } else if (playerChoice === 'Paper' && computerChoice === 'Rock'){
+    console.log(playerChoice);
+    console.log(computerChoice);
+    console.log('You won!');
+    playerScore++;
+    return 'w'
+
+  } else if (playerChoice === 'Scissors' && computerChoice === 'Paper'){
+    console.log(playerChoice);
+    console.log(computerChoice);
+    console.log('You won!');
+    playerScore++;
+    return 'w'
+
+  } else {
+    playAgain = false;
+    console.log(playerChoice);
+    console.log(computerChoice);
+    console.log('You lose!');
+    computerScore++;
+    return 'l'
+  }
+}
+
+function updateScore(result) {
+  if (result === 'w') {
+    pSCore.value = playerScore;
+  } else if (result === 'l') {
+    cSCore.value = computerScore;
+  }
+}
+
+function game(playerChoice) {
+  let result = playRound(playerChoice, getComputerChoice());
+  updateScore(result);
+}
+
 getBtnPlayerChoice();
