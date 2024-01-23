7c524a04383647e7d44bd69524183c63cbcff11d
Patty Lee <seunghak2003@gmail.com>
2024-01-22 19:24:16 -0500

    Finish UI

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const pSCore = document.querySelector('.pSCore');
 const cSCore = document.querySelector('.cSCore');
+const closePopup = document.querySelector('#closePopup');
+const myPopup = document.querySelector('.popup');
+const outcome = document.querySelector('.resultText');
+const pResult = document.querySelector('.playerResult');
+const cResult = document.querySelector('.computerResult');
 playerScore = pSCore.value;
 computerScore = cSCore.value;
 
@@ -23,36 +28,36 @@ function getComputerChoice() {
 
 function playRound(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
-    console.log(playerChoice);
-    console.log(computerChoice);
+    pResult.innerText = playerChoice;
+    cResult.innerText = computerChoice;
     console.log('Tied!');
     return 't'
 
   } else if (playerChoice === 'Rock' && computerChoice === 'Scissors'){
-    console.log(playerChoice);
-    console.log(computerChoice);
+    pResult.innerText = playerChoice;
+    cResult.innerText = computerChoice;
     console.log('You won!');
     playerScore++;
     return 'w'
 
   } else if (playerChoice === 'Paper' && computerChoice === 'Rock'){
-    console.log(playerChoice);
-    console.log(computerChoice);
+    pResult.innerText = playerChoice;
+    cResult.innerText = computerChoice;
     console.log('You won!');
     playerScore++;
     return 'w'
 
   } else if (playerChoice === 'Scissors' && computerChoice === 'Paper'){
-    console.log(playerChoice);
-    console.log(computerChoice);
+    pResult.innerText = playerChoice;
+    cResult.innerText = computerChoice;
     console.log('You won!');
     playerScore++;
     return 'w'
 
   } else {
     playAgain = false;
-    console.log(playerChoice);
-    console.log(computerChoice);
+    pResult.innerText = playerChoice;
+    cResult.innerText = computerChoice;
     console.log('You lose!');
     computerScore++;
     return 'l'
@@ -60,19 +65,28 @@ function playRound(playerChoice, computerChoice) {
 }
 
 function updateScore(result) {
+
   if (result === 'w') {
     pSCore.value = playerScore;
+    outcome.innerText = 'You Won!';
   } else if (result === 'l') {
     cSCore.value = computerScore;
+    outcome.innerText = 'You Lost!';
+  } else if (result === 't') {
+    outcome.innerText = 'You Tied!';
   }
 }
 
 function winCheck() {
+  const displayText = document.querySelector('.displayText');
   if (playerScore === 5) {
+    displayText.innerHTML = 'You Won!';
     myPopup.classList.add("show");
+  } else if (computerScore === 5) {
+    displayText.innerHTML = 'You Lost!';
+    myPopup.classList.add('show');
   }
 }
-
 
 function game(playerChoice) {
   let result = playRound(playerChoice, getComputerChoice());
@@ -80,12 +94,21 @@ function game(playerChoice) {
   winCheck();
 }
 
-getBtnPlayerChoice();
-const closePopup = document.querySelector('#closePopup');
-const myPopup = document.querySelector('#myPopup');
-
 closePopup.addEventListener("click", function (event) {
-  if (event.target == myPopup) {
+  if (event.target == closePopup) {
       myPopup.classList.remove("show");
   }
+  newGame();
 });
+
+function newGame() { 
+  playerScore = 0;
+  computerScore = 0;
+  pSCore.value = playerScore;
+  cSCore.value = computerScore;
+  outcome.innerText = '';
+  pResult.innerText = '';
+  cResult.innerText = '';
+}
+
+getBtnPlayerChoice();
